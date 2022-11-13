1134dbeefb54605c115349000bdfb045ddba051b
MAXTERCAP <75270365+MAXTERCAP@users.noreply.github.com>
2022-11-13 03:59:25 -0300

    TB_LABORATORIOSController_Test

---
+++ FARMACIA--TEST/TB_LABORATORIOController_Test.cs
@@ -0,0 +1,52 @@
+using Farmacia.Controllers;
+using Moq;
+
+
+namespace FARMACIA__TEST
+{
+    public class TB_LABORATORIOController_Test
+    {
+
+        [Fact]
+        public async Task Index_ReturnsAViewResult_WithAListOfLaboratoriosAsync()
+        {
+
+            // Arrange
+            var mockRepo = new Mock<IBrainstormLaboratoriosRepository>();
+            mockRepo.Setup(repo => repo.ListAsync())
+                .ReturnsAsync(GetTestLaboratorios());
+            var controller = new TB_LABORATORIOController(mockRepo.Object);
+
+
+            // Act
+            var result = await controller.Index();
+
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<Laboratorios>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+            Assert.Equal("Bago", model.NOMBRE);
+        }
+
+
+        private List<Laboratorios> GetTestLaboratorios()
+            {
+                var laboratorios = new List<Laboratorios>();
+                laboratorios.Add(new Laboratorios()
+                {
+                    Laboratorio = 1,
+                    NomLab = "Bago"
+                });
+                laboratorios.Add(new Laboratorios()
+                {
+                    Laboratorio = 2,
+                    NomLab = "Bayer"
+                });
+                return laboratorios;
+            }
+
+                
+    }
+}
